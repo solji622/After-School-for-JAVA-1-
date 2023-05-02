6718bf0d3a0e9e6dc07c16c7c91a0af265a0e420
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2023-05-02 17:58:40 +0900

    Update 배열값반복출력.java

--- 배열값반복출력.java
+++ 배열값반복출력.java
@@ -7,12 +7,12 @@ public class Main {
 		
 		int [] arr = new int [5]; // arr라는 배열 안에 5개의 칸 생성
 		int i = 0;
-		while(i < 5) { // i가 4가 될 때까지 반복
+		while(i < arr.length) { // arr.length 로 입력하면 배열 범위가 바뀌어도 자동으로 
 			System.out.print("값을 입력하세요: ");
 			arr [i] = scan.nextInt(); // 배열에 입력 값 저장
 			i++;
 		}
-		for (int j = 0; j < 5; j++) { 
+		for (int j = 0; j < arr.length; j++) { 
 			System.out.println(arr[j]);
 		}
 	}
