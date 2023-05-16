07fd7a539b4a27d1373001563760727bcd31eca1
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2023-05-16 17:31:30 +0900

    Create 아스키코드값총합.java

---
+++ 아스키코드값총합.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Test {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		char a = 'S';
+		char b = 'U';
+		char c = 'N';
+		char d = 'G';
+		char e = 'I';
+		char f = 'L';
+		
+		int sum = a+b+c+d+e+f;
+		// 문자의 아스키코드 값끼리 더함
+		System.out.println(sum);
+		
+		
+	}
+}
