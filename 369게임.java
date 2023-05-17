3aae269cd534795fc5d963d0c9cb627e9fdb822a
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2023-05-17 17:12:15 +0900

    Update 369게임.java

--- 369게임.java
+++ 369게임.java
@@ -1,22 +1,31 @@
 import java.util.Scanner;
 
 public class Test {
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		// 369 게임 33,36,39는 짝짝 출력
 		
-		int num = sc.nextInt();
+		int num;
 		int clap = 0;
+		System.out.print("숫자 입력: ");
+		int last = sc.nextInt();
 		
-		while(true) {
-			if(num % 10 == 3 || num % 10 == 6 || num % 10 == 9) {
-				System.out.println(num +": 짝");
-				clap++;
-			}
-			num = num / 10;
-			if(num == 0) {
-				break;
+		for(int i = 1; i<=last; i++) {
+			num = i;
+			
+			while(true) {
+				if(num % 10 == 3 || num % 10 == 6 || num % 10 == 9) {
+					System.out.println(i +": 짝");
+					clap++; // clap == clap += 1
+				}
+				num = num / 10;
+				if(num == 0) {
+					break;
+				}
 			}
 		}
+		System.out.println("1부터 " + last + "까지의 총 박수: "+clap);
 	}
+
 }
