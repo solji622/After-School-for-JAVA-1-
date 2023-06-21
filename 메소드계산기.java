fa2625eeda854e84b8e802b6c7a9b7f3eca68634
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2023-06-21 17:21:11 +0900

    Create 메소드계산기.java

---
+++ 메소드계산기.java
@@ -0,0 +1,73 @@
+package test;
+
+import java.util.Scanner;
+
+public class TEst {
+
+	public static void main(String[] args) {
+		 // 두 수를 입력받아서
+        Scanner sc = new Scanner(System.in);
+        int num1, num2;
+
+        System.out.print("num1?");
+        num1 = sc.nextInt();
+        System.out.print("num2?");
+        num2 = sc.nextInt();
+       
+        // 사칙연산을 선택하게끔
+        System.out.print("+(1) -(2) *(3) /(4)");
+        int op = sc.nextInt();        
+        double result = 0;
+       
+        // 계산 결과를 출력
+        switch(op) {
+        case 1:
+            result = add(num1, num2);
+            break;
+        case 2:
+        	result = add2(num1, num2);
+            break;
+        case 3:
+        	result = add3(num1, num2);
+            break;
+        case 4:
+            // 더블 = 인트/인트
+        	result = add4(num1, num2);
+            break;
+        default:
+            System.out.print("다시 입력해주세요");        
+        }
+        System.out.println("결과: "+result);
+    }
+    
+	
+	// 리턴값 x == void, 리턴값 o == 해당하는 자료형
+    static int add(int a, int b) {
+    	// a == num1, b == num2
+    	int result;
+        result = a+b;
+        return result;
+    }
+    
+    static int add2(int a, int b) {
+    	// a == num1, b == num2
+    	int result;
+        result = a-b;
+        return result;
+    }
+    
+    static int add3(int a, int b) {
+    	// a == num1, b == num2
+    	int result;
+        result = a*b;
+        return result;
+    }
+    
+    static double add4(int a, int b) {
+    	// a == num1, b == num2
+    	int result;
+        result = a/b;
+        return result;
+    }
+
+}
