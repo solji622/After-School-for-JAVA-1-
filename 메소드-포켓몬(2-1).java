ebd8a1363ad71cdec6076c8fdd8959b87b234ec0
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2023-06-22 18:36:16 +0900

    Create 메소드-포켓몬(2-1).java

---
+++ 메소드-포켓몬(2-1).java
@@ -0,0 +1,13 @@
+package test;
+
+public class Game {
+
+	public static void main(String[] args) {
+		Poketmon p1 = new Poketmon("포켓몬");
+		Poketmon p2 = new Poketmon();
+		System.out.println(p1.pno+":"+p1.name);
+		System.out.println(p2.pno+":"+p2.name);
+
+	}
+
+}
